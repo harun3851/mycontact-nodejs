2bd722eb1053ea08914fb34a4ec12041583de865
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-21 03:40:26 +0300

    Thunder client Setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/api/contacts", (req, res) => {
+    res.status(200).json({message:"get all contacts"});
+});
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+});
