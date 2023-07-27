f69ef8282df5a984b691d28f89b1a1e18fbb9287
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-27 23:38:54 +0300

    contact controller, middleware error, routes

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const dotenv = require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5000;
-
-app.get("/api/contacts", (req, res) => {
-    res.status(200).json({message:"get all contacts"});
-});
+app.use(express.json());
+app.use("/api/contacts",require("./routes/contactRoutes"));
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
