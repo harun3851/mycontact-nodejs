c2f60c2cd312681b92ab613455051d6a8418aaf1
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-30 01:57:34 +0300

    dbConnection added for mongaDB, connection_string for env

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
-
+connectDb();
 const port = process.env.PORT || 5000;
 app.use(express.json());
-app.use("/api/contacts",require("./routes/contactRoutes"));
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
