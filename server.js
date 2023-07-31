42b82c6ea0e06e6707c60051d1fddc92d486fdbf
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-08-01 02:21:30 +0300

    userRoutes created

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const app = express();
 connectDb();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
